545a13a766765d3af8be96be48202701313de374
Subu Kandaswamy <b.kandaswamy@wsu.edu>
2023-02-06 09:25:48 -0800

    Lecture 2/6/2023

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-const a = 10
-let b = 10.05
-let c = 1781718181n
-let d = "hello"
-let e = true
-let f = Symbol("a")
-let g = null
-let h
-
 let o = {
+    v1: 10,
+    v2: 20
+}
+
+let a = 10
+let b = 20
+
+function add1(x, y) {
+    let res = x + y
+    return res
+}
+
+function add2(obj) {
+    let res = obj.v1 + obj.v2
+    return res    
+}
+
+console.log(add1(a,b))
+console.log(add2(o))
+
+let student =  {
     name: "Zack",
-    major: "CS"
+    major: "CS",
+    second_major: null,
+    printDetails: function () {
+        console.log("name: "+this.name);
+        console.log("major:" + this.major);
+    }
 }
 
+student.printDetails();
 
-console.log();
+let res
 
-function add(x, y){
-    return x + y
+console.log(res = console.log("h"))
+
+
+
+if(a > b){
+    res = 1
+} else if (a < b){
+    res = -1
+} else {
+    res = 0
 }
 
-let add =  function(x,y){
-    return x + y
-}
+res = a > b ? 1 : (a < b ? -1 : 0)
